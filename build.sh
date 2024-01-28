5a7ae5296b6c33f1173e31ed1e2687604b9abe8c
kroos <862481966@qq.com>
2024-01-29 00:26:05 +0800

    set enviroment

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-export LD_LIBRARY_PATH=/data/toolchain/lib:$LD_LIBRARY_PATH
 gcc ./src/dcf_test.c ./src/dcf_demo.c ./src/message.c -L ./lib -ldcf  -I ./include -g -o ./build/DCFTestClient -pthread
 cd build&&./DCFTestClient
---
+++ set_env.sh
@@ -0,0 +1,35 @@
+#libzstd
+yum install epel-release.noarch -y
+yum install libzstd.x86_64 -y
+printf "========================================================================"
+printf "libzstd install complete!"
+printf "========================================================================\n"
+
+#libssl
+cd /usr/local/src
+wget https://github.com/openssl/openssl/archive/OpenSSL_1_1_1c.tar.gz
+tar xzvf ./OpenSSL_1_1_1c.tar.gz
+cd openssl-OpenSSL_1_1_1c/
+./config
+make && make install
+export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
+printf "========================================================================"
+printf "libssl install complete!"
+printf "========================================================================\n"
+
+#libcjson
+cd /usr/local/src
+git clone https://github.com/DaveGamble/cJSON.git
+cd cJSON/
+make & make install
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+printf "========================================================================"
+printf "libcjson install complete!"
+printf "========================================================================\n"
+
+#libdcf
+cd /usr/local/src
+git clone https://github.com/fengwenbo7/SsaDCFTest.git
+cd SsaDCFTest
+export LD_LIBRARY_PATH=$(pwd)/lib:$LD_LIBRARY_PATH
+sh build.sh
